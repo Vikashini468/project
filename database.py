21865232aa99bf17c51bb307ba1dc2541186309e
Vikashini468 <gvikashini2410@gmail.com>
2025-02-05 10:58:16 +0530

    database.py

---
+++ database.py
@@ -0,0 +1,28 @@
+import mysql.connector
+class Database:
+    def __init__(self):
+        self.conn=mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="root",
+            database="college"
+        )
+    def addStudent(self,id,name,dept,year,email,password):
+        cur=self.conn.cursor()
+        query="insert into student values(%s,%s,%s,%s,%s,%s)"
+        values=(id,name,dept,year,email,password)
+        cur.execute(query,values)
+        self.conn.commit()
+    def auth(self,email,password):
+        cur = self.conn.cursor()
+        query="select * from student where email=%s and password=%s"
+        values=(email,password)
+        cur.execute(query, values)
+        user=cur.fetchone()
+        return user
+    def getAllUsers(self):
+        cur=self.conn.cursor()
+        query="select * from student"
+        cur.execute(query)
+        users=cur.fetchall()
+        return users
